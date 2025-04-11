45cb5d6921afe38e787152db2c95c07780f038ef
Denzel <denzel.mitski@gmail.com>
2025-04-11 07:30:33 +0200

    feat: add logo

---
+++ client/src/lib/components/visual/Logo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Icon from "./svg/Icon.svelte";
+
+    let { title, fontSize, iconSize } = $props();
+</script>
+
+<div class="flex flex-row gap-x-3">
+    <Icon {iconSize} />
+    <h1 class={`font-head text-primary ${fontSize}`}>{title}</h1>
+</div>
