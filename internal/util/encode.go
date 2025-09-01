4708c0f15bdc78d91eb3ea8584c61a3bad16843c
Denzel <denzel.mitski@gmail.com>
2025-09-01 04:03:57 +0200

    refactor: remove error log

--- internal/util/encode.go
+++ internal/util/encode.go
@@ -3,14 +3,12 @@ package util
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
 
-func Encode[T any](w http.ResponseWriter, status int, v T, err error) error {
+func Encode[T any](w http.ResponseWriter, status int, v T) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	log.Println(err)
 
 	if err := json.NewEncoder(w).Encode(v); err != nil {
 		return fmt.Errorf("failed to encode json: %w", err)
