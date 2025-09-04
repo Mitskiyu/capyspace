e3b0d329cdc7c3123f22a49a2d41a5f0eca3b616
Denzel <denzel.mitski@gmail.com>
2025-09-04 12:10:17 +0200

    feat: add signup fields

--- client/src/components/auth-form.tsx
+++ client/src/components/auth-form.tsx
@@ -1,15 +1,33 @@
 "use client";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import Image from "next/image";
 import { google, logo } from "@/assets";
 import { checkEmail } from "@/lib/auth/check-email";
 import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-const schema = z.object({
-	email: z.email("Email is invalid"),
-});
+const schema = z
+	.object({
+		email: z.email("Email is invalid"),
+		password: z
+			.string()
+			.min(8, "Password must be at least 8 characters")
+			.optional(),
+		confirm: z.string().min(1, "Please confirm your password").optional(),
+	})
+	.refine(
+		(data) => {
+			if (data.password && data.confirm) {
+				return data.password === data.confirm;
+			}
+			return true;
+		},
+		{
+			message: "Passwords do not match",
+			path: ["confirm"],
+		},
+	);
 
 type Inputs = z.infer<typeof schema>;
 type State = "email" | "signup" | "login";
@@ -20,7 +38,6 @@ function AuthForm() {
 	const {
 		register,
 		handleSubmit,
-		clearErrors,
 		formState: { errors, isValid },
 	} = useForm<Inputs>({
 		resolver: zodResolver(schema),
@@ -28,8 +45,15 @@ function AuthForm() {
 	});
 
 	async function onSubmit(data: Inputs) {
-		const exists = await checkEmail(data.email);
-		setState(exists ? "login" : "signup");
+		if (state === "email") {
+			const exists = await checkEmail(data.email);
+			setState(exists ? "login" : "signup");
+			return;
+		}
+
+		if (state === "signup") {
+			// todo
+		}
 	}
 
 	return (
@@ -62,9 +86,15 @@ function AuthForm() {
 					onSubmit={handleSubmit(onSubmit)}
 					className="flex w-full flex-col items-center"
 				>
-					{errors.email && (
+					{errors && (
 						<div className="text-vibrant-coral mt-2 text-sm">
 							{errors.email && <span>{errors.email.message}</span>}
+							{!errors.email && errors.password && (
+								<span>{errors.password.message}</span>
+							)}
+							{!errors.email && !errors.password && errors.confirm && (
+								<span>{errors.confirm.message}</span>
+							)}
 						</div>
 					)}
 					<input
@@ -72,19 +102,34 @@ function AuthForm() {
 						onChange={(e) => {
 							if (state !== "email") setState("email");
 							register("email").onChange(e);
-							clearErrors("email");
 						}}
 						type="text"
 						placeholder="Enter your email"
-						className={`bg-neutrals-surface0 focus:border-vibrant-bloom border-neutrals-overlay0 h-10 w-11/12 rounded-2xl border-1 px-4 py-2 text-base transition-colors duration-300 ease-out placeholder:text-base focus:border-2 focus:outline-none sm:h-12 sm:text-lg sm:placeholder:text-lg ${errors.email ? "border-vibrant-coral mt-2 border-2" : "mt-4"}`}
+						className={`bg-neutrals-surface0 focus:border-vibrant-bloom h-10 w-11/12 rounded-2xl border-1 px-4 py-2 text-base transition-colors duration-300 ease-out placeholder:text-base focus:border-2 focus:outline-none sm:h-12 sm:text-lg sm:placeholder:text-lg ${errors.email || errors.password || errors.confirm ? "mt-2" : "mt-4"} ${state === "email" ? "border-neutrals-overlay0" : "border-transparent"} ${errors.email ? "border-vibrant-coral" : ""}`}
 					/>
+
+					{state === "signup" && (
+						<>
+							<input
+								{...register("password")}
+								type="password"
+								placeholder="Enter your password"
+								className={`bg-neutrals-surface0 focus:border-vibrant-bloom border-neutrals-overlay0 h-10 w-11/12 rounded-2xl border-1 px-4 py-2 text-base transition-colors duration-300 ease-out placeholder:text-base focus:border-2 focus:outline-none sm:h-12 sm:text-lg sm:placeholder:text-lg mt-2 ${errors.password ? "border-vibrant-coral border-2" : ""}`}
+							/>
+							<input
+								{...register("confirm")}
+								type="password"
+								placeholder="Confirm your password"
+								className={`bg-neutrals-surface0 focus:border-vibrant-bloom h-10 w-11/12 rounded-2xl border-1 px-4 py-2 text-base transition-colors duration-300 ease-out placeholder:text-base focus:border-2 focus:outline-none sm:h-12 sm:text-lg sm:placeholder:text-lg mt-1 border-neutrals-overlay0 ${errors.confirm ? "border-vibrant-coral border-2" : ""}`}
+							/>
+						</>
+					)}
 
 					{state === "login" && <p>Login</p>}
 
-					{state === "signup" && <p>Signup</p>}
 					<button
 						disabled={!isValid}
-						className={`border-neutrals-overlay0 mt-2 mb-4 flex h-10 w-11/12 items-center justify-center rounded-2xl border-1 text-base transition duration-200 sm:h-12 sm:text-lg ${
+						className={`mt-2 mb-4 flex h-10 w-11/12 items-center justify-center rounded-2xl  text-base transition duration-200 sm:h-12 sm:text-lg ${
 							isValid
 								? "bg-neutrals-surface0 text-neutrals-text hover:text-vibrant-orchid cursor-pointer hover:opacity-90"
 								: "bg-neutrals-surface0 text-neutrals-text opacity-40"
