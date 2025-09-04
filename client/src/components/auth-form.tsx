dac0a8fb8567f9096b9d804984e2da5c0720d140
Denzel <denzel.mitski@gmail.com>
2025-09-04 06:17:39 +0200

    feat: add auth-form email validation

--- client/src/components/auth-form.tsx
+++ client/src/components/auth-form.tsx
@@ -1,9 +1,39 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import Image from "next/image";
 import { google, logo } from "@/assets";
+import { checkEmail } from "@/lib/auth/check-email";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
+const schema = z.object({
+	email: z.email("Email is invalid"),
+});
+
+type Inputs = z.infer<typeof schema>;
+type State = "email" | "signup" | "login";
 
 function AuthForm() {
+	const [state, setState] = useState<State>("email");
+
+	const {
+		register,
+		handleSubmit,
+		clearErrors,
+		formState: { errors, isValid },
+	} = useForm<Inputs>({
+		resolver: zodResolver(schema),
+		mode: "onTouched",
+	});
+
+	async function onSubmit(data: Inputs) {
+		const exists = await checkEmail(data.email);
+		setState(exists ? "login" : "signup");
+	}
+
 	return (
-		<div className="bg-neutrals-base text-neutrals-text h-86 w-full max-w-78 rounded-4xl sm:h-92">
+		<div className="bg-neutrals-base text-neutrals-text min-h-83 w-full max-w-78 rounded-4xl sm:min-h-90">
 			<div className="flex flex-col items-center">
 				<div className="flex flex-col items-center">
 					<Image
@@ -18,7 +48,7 @@ function AuthForm() {
 						Sign up or log in
 					</h2>
 				</div>
-				<button className="bg-neutrals-surface0 mt-4 flex h-10 w-11/12 transform flex-row items-center justify-center space-x-2 rounded-2xl p-2 transition duration-200 hover:-translate-y-0.5 hover:cursor-pointer hover:opacity-90 active:opacity-90 sm:h-12">
+				<button className="bg-neutrals-surface0 mt-4 flex h-10 w-11/12 transform cursor-pointer flex-row items-center justify-center space-x-2 rounded-2xl p-2 transition duration-200 hover:-translate-y-0.5 hover:opacity-90 active:opacity-90 sm:h-12">
 					<Image src={google} alt="google" width={24} height={24} />
 					<span className="text-lg">Continue with Google</span>
 				</button>
@@ -28,15 +58,41 @@ function AuthForm() {
 						OR
 					</span>
 				</div>
-				<div className="bg-neutrals-surface0 mt-6 flex h-10 w-11/12 rounded-2xl sm:h-12">
+				<form
+					onSubmit={handleSubmit(onSubmit)}
+					className="flex w-full flex-col items-center"
+				>
+					{errors.email && (
+						<div className="text-vibrant-coral mt-2 text-sm">
+							{errors.email && <span>{errors.email.message}</span>}
+						</div>
+					)}
 					<input
+						{...register("email")}
+						onChange={(e) => {
+							if (state !== "email") setState("email");
+							register("email").onChange(e);
+							clearErrors("email");
+						}}
+						type="text"
 						placeholder="Enter your email"
-						className="w-full rounded-2xl bg-transparent px-4 py-2 text-base outline-none placeholder:text-base sm:text-lg sm:placeholder:text-lg"
+						className={`bg-neutrals-surface0 focus:border-vibrant-bloom border-neutrals-overlay0 h-10 w-11/12 rounded-2xl border-1 px-4 py-2 text-base transition-colors duration-300 ease-out placeholder:text-base focus:border-2 focus:outline-none sm:h-12 sm:text-lg sm:placeholder:text-lg ${errors.email ? "border-vibrant-coral mt-2 border-2" : "mt-4"}`}
 					/>
-				</div>
-				<button className="text-neutrals-text bg-neutrals-surface0 hover:text-vibrant-orchid mt-2 flex h-10 w-11/12 flex-row items-center justify-center space-x-2 rounded-2xl transition duration-200 hover:cursor-pointer hover:opacity-90 sm:h-12">
-					<span className="text-base sm:text-lg">Continue</span>
-				</button>
+
+					{state === "login" && <p>Login</p>}
+
+					{state === "signup" && <p>Signup</p>}
+					<button
+						disabled={!isValid}
+						className={`border-neutrals-overlay0 mt-2 mb-4 flex h-10 w-11/12 items-center justify-center rounded-2xl border-1 text-base transition duration-200 sm:h-12 sm:text-lg ${
+							isValid
+								? "bg-neutrals-surface0 text-neutrals-text hover:text-vibrant-orchid cursor-pointer hover:opacity-90"
+								: "bg-neutrals-surface0 text-neutrals-text opacity-40"
+						}`}
+					>
+						Continue
+					</button>
+				</form>
 			</div>
 		</div>
 	);
