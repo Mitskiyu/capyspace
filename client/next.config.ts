2ea59d5a58e760d18ecec4a847bc189ab1ff8382
Denzel <denzel.mitski@gmail.com>
2025-09-04 13:02:53 +0200

    feat: add env build-time validation

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import { z } from "zod";
+
+const schema = z.object({
+	NEXT_PUBLIC_API_URL: z.url(),
+});
+
+const config = schema.safeParse(process.env);
+
+if (!config.success) {
+	console.error("Failed to load env:");
+	console.error(config.error.issues);
+	process.exit(1);
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
 };
 
 export default nextConfig;
