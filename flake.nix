c7bf2c6c643bf8ffbf67395fdd2a26a8702cee1a
Denzel <denzel.mitski@gmail.com>
2026-03-03 12:17:17 +0100

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "capyspace";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        bun
+        go
+      ];
+    };
+  };
+}
